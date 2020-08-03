30f83d8d79ba6d7374f9d15959b1affbaf983cce
jjnote <jungjoon.kim@hotmail.com>
2020-08-03 17:56:48 +0900

    Made major edits on matching Gene ID to Affy ID.

--- Projects/keystone_project/CBD_Keystone_Project.R
+++ Projects/keystone_project/CBD_Keystone_Project.R
@@ -1,5 +1,4 @@
-#This is the keystone project
-#credits: 
+#This is the keystone project.
 
 #Remove all objects.
 rm(list = ls())
@@ -9,8 +8,6 @@ library(graph)
 library(ROntoTools)
 library(KEGGREST)
 library(gprofiler2)
-library(annotate)
-library(hgu133a2.db)
 library(tidyverse)
 library(limma)
 library(edgeR)
@@ -23,35 +20,35 @@ read_rnaseq <- function(rnaseq_file){
     
     rnaseq <- fread(rnaseq_file)
    
-    #change the column name "V1" in rna-seq data to "gene"
+    #Change the column name "V1" in rna-seq data to "gene".
     rnaseq <- setnames(rnaseq, "V1", "gene")
     
-    #remove all strings "?" and zero or more instances of "|." from all elements of rnaseq$gene
+    #Remove all strings "?" and zero or more instances of "|." from all elements of rnaseq$gene.
     rnaseq$gene <- str_remove_all(rnaseq$gene, "\\?")
     rnaseq$gene <- str_remove_all(rnaseq$gene, "\\|.*$")
     
-    #remove all empty spaces for genes
+    #Remove all empty spaces for genes.
     rnaseq[gene == ""] <- NA
     rnaseq <- drop_na(rnaseq, gene)
     
-    #remove any duplicates
+    #Remove any duplicates.
     rnaseq <- distinct(rnaseq, gene, .keep_all = TRUE)
     
-    #from rnaseq$gene, take its column names and make them the rownames of rnaseq 
+    #From rnaseq$gene, take its column names and make them the rownames of rnaseq. 
     rnaseq <- column_to_rownames(rnaseq, var = "gene")
     
     return(rnaseq)
 }
 
-#Use read_rnaseq function to read & clean up the cancer and normal patient's data
+#Use read_rnaseq function to read & clean up the cancer and normal patient's data.
 rnaseq_cancer <- read_rnaseq(str_c(as.character(getwd()),"/Data/RNASeq_PAAD"))
 rnaseq_normal <- read_rnaseq(str_c(as.character(getwd()),"/Data/RNASeq_PAAD_Normal"))
 
 #Combine cancer and normal data into one. 
 rnaseq_all <- cbind(rnaseq_cancer,rnaseq_normal)
 
-#since duplicate names will happen for patients with normal data, add string "-NORMAL" to them. 
-#Then assign those names back to column of rnaseq_all
+#Since duplicate names will happen for patients with normal data, add string "-NORMAL" to them. 
+#Then assign those names back to column of rnaseq_all.
 temp <- as.vector(colnames(rnaseq_all))
 colnames(rnaseq_all) <- make.unique(temp, sep = "_Normal")
 colnames(rnaseq_all) <- str_replace_all(colnames(rnaseq_all),"_Normal1","-Normal")
@@ -80,43 +77,43 @@ temp <- contrasts.fit(fit, contr)
 temp <- eBayes(temp)
 degTable <- topTable(temp, sort.by = "P", number = Inf)
 
-#We are done with preprocessing with edgeR/limma + voom since we have a DEGTable. 
-
-
-#More preprocessing: We need to map KEGG pathway to Entrez id, but input TCGA data is in HGNC gene symbols.
+#We are done with edgeR/limma + voom since we have a DEGTable.
+#More pre-processing: We need to map KEGG pathway to Entrez id, but input TCGA data is in HGNC gene symbols.
 #First, convert HGNC gene symbol to Entrez id using gprofiler2 package.
-entrez_id <- as.vector(gconvert(query = (as.vector(rownames(rnaseq_all))), organism = "hsapiens", target = "ENTREZGENE_ACC", mthreshold = Inf, filter_na = TRUE)$target)
+entrez_id <- as.vector(gconvert(query = (as.vector(rownames(rnaseq_all))), organism = "hsapiens", target = "ENTREZGENE_ACC", mthreshold = Inf, filter_na = FALSE)$target)
 
 #Transform Entrez to KEGG genes by putting on a "hsa:".
 kegg_gene_id <- paste0("hsa:",entrez_id)
 
-#work in progress: bind KEGG_gene_id to DEGTable
-#DEGTable <- rbind.fill(as.data.frame(t(DEGTable)),as.data.frame(t(KEGG_gene_id)))
-#set column name to "entrez"
+#Add a column named "entrez" with the KEGG gene id's in degTable.
 
-#Now we make the gene names of DEGTable in to probe names of affymetrix. 
-#gene_list <- rownames(degTable)
-#mapped_probes <- mappedkeys(hgu133a2SYMBOL)
-#probe_n_sym <- as.data.frame(hgu133a2SYMBOL[mapped_probes])
-#result <- probe_n_sym[which(probe_n_sym$symbol %in% gene_list),]
-#result_1 <- result[match(gene_list,result$symbol),]
 
+#Now set TCGA gene name to Affy Probe ID's. 
+#Note: didn't filter for sensitivity or specificity.
 gene_list <- rownames(degTable)
 gene_list <- gene_list %>% str_remove_all("[:blank:]") %>% str_remove_all("\\-AS[:digit:]") %>% str_remove_all("\\-AS") %>% str_remove_all("\\-") %>% str_to_upper()
 mapped_probes <- read_tsv(str_c(as.character(getwd()),"/Data/GeneAnnot_191128_unmerge.tsv"))
+
 mapped_probes <- mapped_probes %>% 
 dplyr::select(c(AffyID, GeneSymbol, Sensitivity, Specificity)) %>%
-dplyr::rename(Gene_Name = GeneSymbol) %>% 
-drop_na(c(Sensitivity, Specificity, Gene_Name)) 
-#didn't filter for sensitivity or specificity
+dplyr::rename(gene_name = GeneSymbol) %>% 
+drop_na(c(Sensitivity, Specificity, gene_name)) 
+mapped_probes$gene_name <- mapped_probes$gene_name %>% str_remove_all("[:blank:]") %>% str_remove_all("\\-AS[:digit:]") %>% str_remove_all("\\-AS") %>% str_remove_all("\\-") %>% str_to_upper()
 
-mapped_probes$Gene_Name <- mapped_probes$Gene_Name %>% str_remove_all("[:blank:]") %>% str_remove_all("\\-AS[:digit:]") %>% str_remove_all("\\-AS") %>% str_remove_all("\\-") %>% str_to_upper()
-probe_n_sym <- cbind(mapped_probes$AffyID,mapped_probes$Gene_Name)
-probe_n_sym <- as.data.frame(probe_n_sym)
-result <- probe_n_sym[which(probe_n_sym$V2 %in% gene_list),]
-result_1 <- result[match(gene_list,result$V2),]
+#Now set rownames of degTable to AffyID.
+temp <- cbind(mapped_probes$AffyID,mapped_probes$gene_name) %>% as.data.frame()
+row_names <- temp[which(temp$V2 %in% gene_list),]
+rm(temp)
+row_names <- row_names[match(gene_list,row_names$V2),]$V1
+row_names <- make.names(row_names, unique = TRUE)
 
-#do separate_rows(불러온데이터, GeneSymbol, sep = "[:blank:]" or sep = "") in above?
+#Clean up row names of degTable.
+row.names(degTable) <- row_names 
+base::rownames(degTable) <- str_remove_all(base::rownames(degTable),"^X")
+degTable <- degTable[base::grep("NA.", base::rownames(degTable), invert = TRUE),] 
+
+#Now select relevant columns for degTable. 
+degTable <- degTable %>% dplyr::select("logFC","P.Value","adj.P.Val", "entrez")
 
 #Then, map Entrez id to KEGG hsa (homo sapiens) pathways using KEGGREST package.
 #When supplying KEGG_gene_id at once, it cannot handle the amount of inputs. In fact,
