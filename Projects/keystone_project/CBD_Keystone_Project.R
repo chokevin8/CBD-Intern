864d551acc168d7c0f030ce6381e06d31451c8dc
jjnote <jungjoon.kim@hotmail.com>
2020-08-07 11:55:16 +0900

    Made major edits.

--- Projects/keystone_project/CBD_Keystone_Project.R
+++ Projects/keystone_project/CBD_Keystone_Project.R
@@ -7,7 +7,6 @@ rm(list = ls())
 library(graph)
 library(ROntoTools)
 library(KEGGREST)
-library(gprofiler2)
 library(GeneBook)
 library(tidyverse)
 library(limma)
@@ -78,68 +77,66 @@ temp <- contrasts.fit(fit, contr)
 temp <- eBayes(temp)
 degTable <- topTable(temp, sort.by = "P", number = Inf)
 
-#Take out genes that contain "LOC" in degTable: (problem: what if there are other genes that contain "LOC"?)
+#Take out genes that contain "LOC" in degTable: 
 degTable <- dplyr::filter(degTable, !grepl("LOC", rownames(degTable)))
 
 #We are done with edgeR/limma + voom since we have a DEGTable.
-#Now, convert input TCGA gene symbol to Entrez id using GeneCard and gprofiler2 package. 
-
+#Now, convert input TCGA gene symbol to Entrez id using GeneCard package. 
+    
 gc_list <- as.data.frame((GeneBook::genecard_id))
-gc_list<- dplyr::filter(gc_list, !grepl("Ensembl",gc_list$subname))
-gc_list <- dplyr::filter(gc_list, !grepl("HGNC", gc_list$subname))
-gc_list <- dplyr::filter(gc_list, !grepl("LOC", gc_list$subname))
-#gc_list <- str_remove_all(gc_list$subname, "[:blank:]")
+gc_list <- dplyr::filter(gc_list, grepl("Entrez", gc_list$subname))
+gc_list$subname <- gsub('"','',gc_list$subname)
+gc_list$subname <- str_replace_all(gc_list$subname, "Entrez Gene: ", "hsa:")
+base::colnames(gc_list) <- c("gene_symbol", "entrez")
+
+degTable <- degTable %>% select("logFC","P.Value","adj.P.Val") %>% rownames_to_column(var = "gene_symbol") %>%
+    inner_join(gc_list, by = "gene_symbol") %>% column_to_rownames(var = "gene_symbol")
+
+#a <- alias2Symbol(rownames(degTable, species = "Hs"))
+
 
 #.SD = subsets all of gc_list except gene (so subname) specified in the "by" argument, and we use lapply to paste the characters together for 
 #subnames and group it by the gene names. 
-gc_list <- as.data.table(gc_list)
-collapsed_gc_list <- gc_list[, base::lapply(.SD, paste0, collapse=" "), by=gc_list$gene]
-collapsed_gc_list <- collapsed_gc_list %>% as.matrix() %>% str_remove_all("[:blank:]") %>% str_to_upper()
+#gc_list <- as.data.table(gc_list)
+#collapsed_gc_list <- gc_list[, base::lapply(.SD, paste0, collapse=" "), by=gc_list$gene]
+#collapsed_gc_list <- collapsed_gc_list %>% as.matrix() %>% str_remove_all("[:blank:]") %>% str_to_upper()
 
 #Now match gc_list to rownames(degTable)
-temp <- rownames(degTable)
-temp <- str_to_upper(temp)
-temp <- as.data.frame(temp)
-colnames(temp) <- "V1"
+#temp <- rownames(degTable)
+#temp <- str_to_upper(temp)
+#temp <- as.data.frame(temp)
+#colnames(temp) <- "V1"
+#a <- temp[match(temp$V1,collapsed_gc_list),] 
+#temp <- c(temp)
 
-a <- temp[match(temp$V1,collapsed_gc_list),]
-
-entrez <- as.vector(gconvert(query = (as.vector(rownames(degTable))), organism = "hsapiens", target = "ENTREZGENE_ACC", mthreshold = Inf, filter_na = FALSE)$target)
-
-#Transform Entrez to usable form by putting on a "hsa:".
-entrez <- paste0("hsa:",entrez)
-
-#Add a column named "entrez" with the KEGG gene id's in degTable.
-degTable <- cbind(degTable, entrez, make.row.names = TRUE)
+#entrez <- as.vector(gconvert(query = temp, organism = "hsapiens", target = "ENTREZGENE_ACC", mthreshold = Inf, filter_na = TRUE)$target)
+#entrez_a <- as.vector(gconvert(query = a, organism = "hsapiens", target = "ENTREZGENE_ACC", mthreshold = Inf, filter_na = TRUE)$target)
 
 #Now set TCGA gene name to Affy Probe ID's. 
 #Note: didn't filter for sensitivity or specificity.
-gene_list <- rownames(degTable)
-gene_list <- gene_list %>% str_remove_all("[:blank:]") %>% str_remove_all("\\-AS[:digit:]") %>% str_remove_all("\\-AS") %>% str_remove_all("\\-") %>% str_to_upper()
+#gene_list <- rownames(degTable)
+#gene_list <- gene_list %>% str_remove_all("[:blank:]") %>% str_remove_all("\\-AS[:digit:]") %>% str_remove_all("\\-AS") %>% str_remove_all("\\-") %>% str_to_upper()
 
 #read probe data (annotation):
-mapped_probes <- read_tsv(str_c(as.character(getwd()),"/Data/GeneAnnot_191128_unmerge.tsv"))
+#mapped_probes <- read_tsv(str_c(as.character(getwd()),"/Data/GeneAnnot_191128_unmerge.tsv"))
 
-mapped_probes <- mapped_probes %>% 
-dplyr::select(c(AffyID, GeneSymbol, Sensitivity, Specificity)) %>%
-dplyr::rename(gene_name = GeneSymbol) %>% 
-drop_na(c(Sensitivity, Specificity, gene_name)) 
-mapped_probes$gene_name <- mapped_probes$gene_name %>% str_remove_all("[:blank:]") %>% str_remove_all("\\-AS[:digit:]") %>% str_remove_all("\\-AS") %>% str_remove_all("\\-") %>% str_to_upper()
+#mapped_probes <- mapped_probes %>% 
+#dplyr::select(c(AffyID, GeneSymbol, Sensitivity, Specificity)) %>%
+#dplyr::rename(gene_name = GeneSymbol) %>% 
+#drop_na(c(Sensitivity, Specificity, gene_name)) 
+#mapped_probes$gene_name <- mapped_probes$gene_name %>% str_remove_all("[:blank:]") %>% str_remove_all("\\-AS[:digit:]") %>% str_remove_all("\\-AS") %>% str_remove_all("\\-") %>% str_to_upper()
 
 #Now set rownames of degTable to AffyID.
-temp <- cbind(mapped_probes$AffyID,mapped_probes$gene_name) %>% as.data.frame()
-row_names <- temp[which(temp$V2 %in% gene_list),]
-rm(temp)
-row_names <- row_names[match(gene_list,row_names$V2),]$V1
-row_names <- make.names(row_names, unique = TRUE)
-row.names(degTable) <- row_names 
+#temp <- cbind(mapped_probes$AffyID,mapped_probes$gene_name) %>% as.data.frame()
+#row_names <- temp[which(temp$V2 %in% gene_list),]
+#rm(temp)
+#row_names <- row_names[match(gene_list,row_names$V2),]$V1
+#row_names <- make.names(row_names, unique = TRUE)
+#row.names(degTable) <- row_names 
 
 #Clean up row names of degTable.
-base::rownames(degTable) <- str_remove_all(base::rownames(degTable),"^X")
-degTable <- degTable[base::grep("NA.", base::rownames(degTable), invert = TRUE),] 
-
-#Now select relevant columns for degTable. 
-degTable <- degTable %>% dplyr::select("logFC","P.Value","adj.P.Val", "entrez")
+#base::rownames(degTable) <- str_remove_all(base::rownames(degTable),"^X")
+#degTable <- degTable[base::grep("NA.", base::rownames(degTable), invert = TRUE),] 
 
 #Then, map Entrez id to KEGG hsa (homo sapiens) pathways using KEGGREST package.
 #When supplying KEGG_gene_id at once, it cannot handle the amount of inputs. In fact,
@@ -155,18 +152,19 @@ degTable <- degTable %>% dplyr::select("logFC","P.Value","adj.P.Val", "entrez")
 #}
 #temp_list <- keggLink("pathway", entrez)
     
-
 #Now we download and parse homo sapiens keggPathways using kpg.
-kpg <- keggPathwayGraphs("hsa", updateCache = TRUE, verbose = TRUE)
+kpg <- keggPathwayGraphs("hsa", updateCache = FALSE, verbose = TRUE)
+kpg <- setEdgeWeights(kpg, edgeTypeAttr = "subtype", edgeWeightByType = list(activation = 1, inhibition = -1, expression = 1, repression = -1), defaultWeight = 0)
+pv <- degTable$P.Value
+kpg <- setNodeWeights(kpg, weights = alphaMLG(pv))
 
 #Store pathway names in kpn.
-#kpn <- keggPathwayNames("hsa", updateCache = TRUE, verbose = TRUE)
+kpn <- keggPathwayNames("hsa", updateCache = FALSE, verbose = TRUE)
 
 #Finally perform primary disregulation (pDis).
 pDisRes <- pDis(x = degTable$logFC, graphs = kpg, ref = degTable$entrez, nboot = 2000, verbose = FALSE )
 
-
 #We have the result of most perturbed pathways ordered by pValue. Stored in "Result". 
-#result <- Summary(pDisRes, pathNames = kpn, totalpDis = FALSE, pORA = FALSE, comb.pv = NULL, order.by = "ppDis")
+result <- Summary(pDisRes, pathNames = kpn, totalpDis = FALSE, pORA = FALSE, comb.pv = NULL, order.by = "ppDis")
 
 
